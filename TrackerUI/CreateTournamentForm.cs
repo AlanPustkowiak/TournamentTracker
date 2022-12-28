7fdef674c28b03a875dd41a37fdee851eb79ab8c
alanp <alanp@STOWAPC>
2022-12-29 00:25:31 +0100

    Making logic for rounds

--- TrackerUI/CreateTournamentForm.cs
+++ TrackerUI/CreateTournamentForm.cs
@@ -126,7 +126,7 @@ namespace TrackerUI
             tm.Prizes = selectedPrizes;
             tm.EnteredTeams = selectedTeams;
 
-
+            TournamentLogic.CreateRounds(tm);
 
             GlobalConfig.Connection.CreateTournament(tm);
         }
